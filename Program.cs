508b7a3619c563c12ccdade6ab8b5922afe3a914
hitontwo2 <hitontwo2@gmail.com>
2024-10-21 20:37:48 +0530

    Video

--- Program.cs
+++ Program.cs
@@ -2,26 +2,19 @@
 using Emgu.CV;
 using Emgu.CV.Structure;
 using System.Drawing;
-class Program{
-
-    static void Main(string[] args){
-
-        string image_path = @"./eren1.jpeg";
-
-        Mat image = CvInvoke.Imread(image_path, Emgu.CV.CvEnum.ImreadModes.Color);
-
-        int ratio= image.Size.Width / image.Size.Height;
-        int newHeight = Console.WindowHeight;
-
-        int newWidth = ratio * newHeight;
-        Mat resizedImage = new Mat();
+using System.Diagnostics;
+class Program
+{
+    static string Converter(Mat source, int Height, int Width)
+    {
         List<string> Output = new List<string>();
-        CvInvoke.Resize(image,resizedImage, new Size(newWidth, newHeight) );
-        Image<Bgr, byte> ReadImage = resizedImage.ToImage<Bgr, byte>();
-        for (int y = 0; y < newHeight; y++){
+        Image<Bgr, byte> ReadImage = source.ToImage<Bgr, byte>();
+        for (int y = 0; y < Height; y++)
+        {
             string layer = "";
-            for (int x = 0;x < newWidth; x++) {
-                Bgr color = ReadImage[y,x];
+            for (int x = 0; x < Width; x++)
+            {
+                Bgr color = ReadImage[y, x];
                 string Pixel = $"\x1b[48;2;{(int)(color.Red)};{(int)(color.Green)};{(int)(color.Blue)}m  \x1b[0m";
 
                 layer += Pixel;
@@ -29,9 +22,32 @@ class Program{
             }
             Output.Add(layer);
         }
-        Console.WriteLine(Output);
         string Resultant = String.Join("\n", Output);
-        Console.WriteLine(Resultant);
-        CvInvoke.WaitKey(0);
+        return Resultant;
+    }
+
+    static void Main(string[] args)
+    {
+        VideoCapture video = new VideoCapture("./video.mp4");
+        double fps = video.Get(Emgu.CV.CvEnum.CapProp.Fps);
+        int delay = (int)(1000/fps);
+        Mat image = new Mat();
+        while (video.Read(image))
+        {
+            double ratio = (double)(image.Size.Width / image.Size.Height);
+            int newHeight = Console.WindowHeight;
+
+            int newWidth = (int)(ratio * newHeight);
+            Mat resizedImage = new Mat();
+            CvInvoke.Resize(image, resizedImage, new Size(newWidth, newHeight));
+
+
+            string output = Converter(resizedImage, newHeight, newWidth);
+            Console.SetCursorPosition(0, 0);
+            Console.Write(output);
+            Thread.Sleep(delay);
+            CvInvoke.WaitKey(0);
+        }
+
     }
 }
